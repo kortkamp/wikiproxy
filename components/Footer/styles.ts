12c0af63ec540dbdadfc84b66f61e87530fdb777
kortkamp <marcelusmedius@gmail.com>
2021-11-28 18:08:25 -0300

    feat: add basic footer

---
+++ components/Footer/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: ${({theme})=>theme.colors.text};
+  background-color:#f0f0f0;
+`;
