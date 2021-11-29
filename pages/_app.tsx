ea54764ddd95abb52aa4a7d136b4f57c26803569
kortkamp <marcelusmedius@gmail.com>
2021-11-29 14:08:01 -0300

    refactor: add DataProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,23 +6,25 @@ import useDarkMode from 'use-dark-mode';
 import GlobalStyle from '../styles/global';
 import {lightTheme, darkTheme} from '../styles/theme';
 
+import { DataProvider } from '../hooks/useData';
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [isMounted, setIsMounted] = useState(false)
   const darkmode = useDarkMode();
 
   useEffect(() => {
     setIsMounted(true)
+    
   }, [])
   return (
-    <>
-    <ThemeProvider theme={darkmode.value ? darkTheme : lightTheme} >
-      <GlobalStyle />
-      
-      {isMounted &&  
-        <Component {...pageProps} />
-      }
-    </ThemeProvider>
-    </>
+    <DataProvider>
+      <ThemeProvider theme={darkmode.value ? darkTheme : lightTheme} >
+        <GlobalStyle />
+        {isMounted &&  
+          <Component {...pageProps} />
+        }
+      </ThemeProvider>
+    </DataProvider>
   )
 }
 
