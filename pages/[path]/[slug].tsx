9d275200667ac81e34b3eef7167fa9d288ee6a3e
kortkamp <marcelusmedius@gmail.com>
2021-11-29 14:10:07 -0300

    refactor: split code from ssr

--- pages/[path]/[slug].tsx
+++ pages/[path]/[slug].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import axios from 'axios';
@@ -8,6 +8,8 @@ import { Header } from "../../components/Header";
 const WikiSection = dynamic(() => import("../../components/WikiSection"));
 
 import {Container} from '../../styles/Article';
+import { useData } from "../../hooks/useData";
+import * as services from "../../services";
 
 interface PropsMobileView {
   stringPath: string;
@@ -34,17 +36,25 @@ interface SectionsData {
 }
 
 interface Props {
-  stringPath: string;
-  stringSlug:string;
-  data:{ 
-    parse:  { text:string}
-  };
-  sectionsData:SectionsData;
+  language: string;
+  articleName:string;
+  httpStatus: number;
+  htmlData: any;
+  sectionsData: SectionsData;
 }
 
-const Post = ({stringPath,stringSlug,data,sectionsData}:Props) => {
+const Post = ({
+  language,
+  articleName,
+  httpStatus,
+  htmlData,
+  sectionsData
+}:Props) => {
 
-  const [language, setLanguage] = useState(stringPath);
+  const { initializeInClientSide,setUserData } = useData();
+
+  const pageFound = httpStatus === 200;
+
 
   return (
     <>
@@ -54,47 +64,82 @@ const Post = ({stringPath,stringSlug,data,sectionsData}:Props) => {
       
       {/* <BackgroundImage page={stringSlug} lang={stringPath}></BackgroundImage> */}
       <Header language={language}/>
-      <Container>
-        <Toc data= { sectionsData.parse } />
-        <div className="contentWraper article_content">
-          <div className='title'>
-            {sectionsData.parse.title}
+      {
+        pageFound ?
+        <Container>
+          <Toc data= { sectionsData.parse } />
+          <div className="contentWraper article_content">
+            <div className='title'>
+              {sectionsData.parse.title}
+            </div>
+            <div id="overview" className="article_content" dangerouslySetInnerHTML={{ __html: htmlData }} ></div>
+          
+            { sectionsData.parse.sections
+            .filter((section)=>section.byteoffset > 0)
+            .map((section)=>(
+                <WikiSection key={section.index} lang={language} page={articleName} section={section}/>    
+            ))}
           </div>
-          <div id="overview" className="article_content" dangerouslySetInnerHTML={{ __html: data.parse.text['*'] }} ></div>
-         
-          {sectionsData.parse.sections.filter((section)=>section.byteoffset > 0)
-          .map((section)=>(
-              <WikiSection key={section.index} lang={stringPath} page={stringSlug} section={section}/>    
-          ))}
-        </div>
-      </Container>
+        </Container>
+        :
+        <Container>
+          <div className="contentWraper article_content">
+            <h1>
+              404 - Not Found
+            </h1>
+          </div>
+        </Container>
+
+      }
+      
     </>
   )
 }
 
 export const getServerSideProps = async ({query}) => {
+  const { slug , path} = query;
+
+  const articleName = slug as string;
+  const language = path as string; 
+
+  const responseData = await services.GetFirstSection(language,articleName);
+  
+  return {
+    props: {language, articleName , ...responseData}, 
+  }
+}
+
+
+
+
+
+export const __getServerSideProps = async ({query}) => {
   
   const { slug , path} = query;
 
   let stringPath = path as string;
+
+  let language = path as string; 
   if(stringPath === 'wiki'){
-    stringPath = 'en';
+    language = 'en';
   }
   const stringSlug = slug as string;
 
-  const url = `https://${stringPath}.wikipedia.org/w/api.php?action=parse&page=${encodeURI(stringSlug)}&mobileformat=&prop=text&section=0&format=json&effectivelanglinks=&redirects=`
- 
-  const sectionsUrl = `https://${stringPath}.wikipedia.org/w/api.php?action=parse&page=${encodeURI(stringSlug)}&prop=sections&format=json`
+  const url = `https://${language}.wikipedia.org/w/api.php?action=parse&page=${encodeURI(stringSlug)}&mobileformat=&prop=text&section=0&format=json&effectivelanglinks=&redirects=`
+  const sectionsUrl = `https://${language}.wikipedia.org/w/api.php?action=parse&page=${encodeURI(stringSlug)}&prop=sections&format=json`
 
   try{
     const {data:sectionsData} = await axios.get(sectionsUrl);
-    const  {data} = await axios.get(url);
+    const  { data } = await axios.get(url);
+
+    const wikiData = data.parse.text['*'].replace(/\/wiki\//g, `/${language}/`);
+    
     return {
-      props: {stringPath,stringSlug, data, sectionsData}, // will be passed to the page component as props
+      props: {stringPath,stringSlug, data: wikiData , sectionsData}, // will be passed to the page component as props
     }
   }
   catch (err){
-    
+    console.log(err)
   }
 
   return {
