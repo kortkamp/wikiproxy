1fe63573ab5c3df1ec095f6ef707e49cb1f554d2
kortkamp <marcelusmedius@gmail.com>
2021-12-01 12:03:57 -0300

    fix: add theme to background color

--- styles/Home.ts
+++ styles/Home.ts
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   margin-top: 0 auto;
+
+  background-color: ${(props)=> props.theme.colors.background};
   
 
-  `;
+`;
 
 export const Content = styled.div`  
   width: 100%;
